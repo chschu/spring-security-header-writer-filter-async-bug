6c78da7947a3e3341e2dd40c26527de55c5b7a1c
Christian Schuster <christian@dnup.de>
2024-08-02 18:22:27 +0200

    add workaround

--- src/main/java/org/example/SampleController.java
+++ src/main/java/org/example/SampleController.java
@@ -1,12 +1,15 @@
 package org.example;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class SampleController {
@@ -18,6 +21,12 @@ public class SampleController {
 
 	@GetMapping("/async")
 	public StreamingResponseBody getAsync() {
+		return outputStream -> outputStream.write("Hello, World!\n".getBytes(StandardCharsets.UTF_8));
+	}
+
+	@GetMapping("/async-workaround")
+	public StreamingResponseBody getAsyncWorkaround(HttpServletResponse response) throws IOException {
+		response.flushBuffer();
 		return outputStream -> outputStream.write("Hello, World!\n".getBytes(StandardCharsets.UTF_8));
 	}
 }
